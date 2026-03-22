f45749fc66d958655e0e21fbd13e9ab34bddd50a
Bao Nguyen <baontq23@gmail.com>
2026-03-22 17:32:18 +0700

    chore: add eslint and prettier configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  {
+    ignores: ["dist/", "node_modules/", "bin/"],
+  }
+);
